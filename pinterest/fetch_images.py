e40a6d1bc48bf5b264dbdb1dc7968ab295802e51
Hector Yee <hector.yee@gmail.com>
2022-10-23 16:00:23 -0700

    Completed fetch images

--- pinterest/fetch_images.py
+++ pinterest/fetch_images.py
@@ -6,9 +6,11 @@
 """
   Fetches images from pinterest to a local directory.
 """
-import random
+import time
 import json
 from typing import FrozenSet
+import os
+import urllib.request
 
 from absl import app
 from absl import flags
@@ -18,6 +20,7 @@ import pin_util
 FLAGS = flags.FLAGS
 _INPUT_FILE = flags.DEFINE_string("input_file", None, "Input json file.")
 _MAX_LINES = flags.DEFINE_integer("max_lines", 1000, "Max lines to read")
+_SLEEP_TIME = flags.DEFINE_float("sleep_time", 0.1, "Sleep time in seconds.")
 _OUTPUT_DIR = flags.DEFINE_string("output_dir", None, "The output directory.")
 
 # Required flag.
@@ -42,6 +45,17 @@ def get_keys(input_file: str, max_lines: int) -> FrozenSet[str]:
     result = frozenset(keys)
     return result
 
+def fetch_image(key: str, output_dir: str) -> None:
+    """Fetches an image from pinterest."""
+    output_name = os.path.join(output_dir, "%s.jpg" % key)
+    if os.path.exists(output_name):
+        print("%s already downloaded." % key)
+        return
+    url = pin_util.key_to_url(key)
+    with urllib.request.urlopen(url) as response:
+        with open(output_name, "wb") as f:
+            f.write(response.read())
+    
 
 def main(argv):
     """Main function."""
@@ -49,6 +63,9 @@ def main(argv):
 
     keys = get_keys(_INPUT_FILE.value, _MAX_LINES.value)
     print("Found %d unique images to fetch" % len(keys))
+    for key in keys:
+        fetch_image(key, _OUTPUT_DIR.value)
+        time.sleep(_SLEEP_TIME.value)
 
 if __name__ == "__main__":
     app.run(main)
